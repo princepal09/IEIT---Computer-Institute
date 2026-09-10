9e1f83861890ba86ffcf34e8d088f423778a65d4
Prince Pal <princepal262501@gmail.com>
2026-09-10 23:38:33 +0530

    fix: update SameSite attribute for cookies to handle cross-site requests in production

--- backend/src/utils/auth.helper.ts
+++ backend/src/utils/auth.helper.ts
@@ -27,30 +27,29 @@ export const setCookies = (res: Response, accessToken: string, refreshToken: str
   res.cookie('accessToken', accessToken, {
     httpOnly: true,
     secure: NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 15 * 60 * 1000,
   });
 
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
     secure: NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
-
 export const destroyCookie = (res: Response) => {
   res.clearCookie('accessToken', {
     httpOnly: true,
     secure: NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 15 * 60 * 1000,
   });
 
   res.clearCookie('refreshToken', {
     httpOnly: true,
     secure: NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
